a654ebdb286bde9fc8cc409884009d0bdbc52bc3
John Smith <roytd56@gmail.com>
2020-04-23 20:10:52 +0700

    sempurna

--- answer3.php
+++ answer3.php
@@ -5,7 +5,7 @@ $angka1=array(1,2,3,4,5,6,7,8,9,10,11,12,13,15);
 
 for($i=0; $i < count($angka); $i++){
       $hasil[$i]=$angka[$i] * $angka1[$i];
-      echo $hasil[$i]. "</br>";
+      echo "$angka[$i] * $angka1[$i]=$hasil[$i]". "</br>";
 }
 
 ?>
